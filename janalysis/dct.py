b0fdc88862cde6be87c4e0b8fc53ae30c955a1fe
Andrew Halaney <ajhalaney@gmail.com>
2018-03-22 17:11:07 -0500

    Implement one-dimension dct2

--- janalysis/dct.py
+++ janalysis/dct.py
@@ -1,6 +1,25 @@
-"""Implements the DCT (in the future)."""
+"""Implements the DCT."""
+from math import cos, pi
+import numpy
 
 
-def temp_function():
-    """Temporary function to test imports."""
-    print('Temporary!')
+def dct2(x_list, n_point=None):
+    """Implements the n_point dct2.
+    :param x_list: The time domain sequence.
+    :type x_list: list.
+    :param n_point: The n point size of the DCT to be performed (default None
+                    is to use the size of the input).
+    :type n_point: int.
+    :returns: array -- the transform representation of the sequence.
+    """
+    if not n_point:
+        n_point = len(x_list)
+    x_array = numpy.array(x_list)
+    y_array = numpy.zeros(n_point)
+    for index_k in range(n_point):
+        for index_n in range(n_point):
+            if index_n < x_array.size:
+                temp = cos(pi * index_k * (2 * index_n + 1) / (2 * n_point))
+                y_array[index_k] += x_array[index_n] * temp
+        y_array[index_k] *= 2
+    return y_array
--- tests/test_dct.py
+++ tests/test_dct.py
@@ -1,8 +1,36 @@
 """Test the DCT implementation."""
-from janalysis.dct import temp_function
+import numpy
+import scipy.fftpack
+from janalysis.dct import dct2
 
 
-def test_temporary_function():
-    """Prove that importing from project is working."""
-    temp_function()
-    assert True
+def test_1d_dct2_simple():
+    """Tests the 1D version of the dct2."""
+    time_domain = [1, 1, 1, 1]
+    expected = scipy.fftpack.dct(time_domain)
+    actual = dct2(time_domain)
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_1d_dct2_simple_specific_length():
+    """Tests the 1D version of the dct2 with n_point of equal length."""
+    time_domain = [1, 1, 1, 1]
+    expected = scipy.fftpack.dct(time_domain)
+    actual = dct2(time_domain, len(time_domain))
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_1d_dct2_smaller_length():
+    """Tests the 1D version of the dct2 with n_point less than input length."""
+    time_domain = [1, 1, 1, 1]
+    expected = scipy.fftpack.dct(time_domain, n=3)
+    actual = dct2(time_domain, 3)
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_dct2_bigger_length():
+    """Tests the 1D version of the dct2 with n_point greater than length."""
+    time_domain = [1, 1, 1, 1]
+    expected = scipy.fftpack.dct(time_domain, n=8)
+    actual = dct2(time_domain, 8)
+    numpy.testing.assert_array_almost_equal(actual, expected)
