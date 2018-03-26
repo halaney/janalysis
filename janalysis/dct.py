b8abfd33495b607c6674dca2ad83a121e6a0dcb3
Andrew Halaney <ajhalaney@gmail.com>
2018-03-25 21:14:36 -0500

    Implement 2d orthonormal dct
    Implemented the 2d orthormal dct using the help of these pages
    https://docs.scipy.org/doc/scipy/reference/generated/scipy.fftpack.dct.html
    https://en.wikipedia.org/wiki/Discrete_cosine_transform
    https://en.wikipedia.org/wiki/JPEG#Discrete_cosine_transform
    
    Some parts of the 2d transform I did not understand. I first implemented
    the one from the JPEG link above, but I decided I wanted one that wasn't
    limited to just 8x8 matrices. I then tried using the wikipedia description
    and scipy descriptions, and got close, but was off by a factor of 4. I fixed
    that without understanding it and now it works. I tested it with different
    shape and frequency matrices hoping to catch a case where it didn't work, but
    I never found one (thank god for pytest). It doesn't really matter anyways
    since I planned on just implementing JPEG with it, but I wanted to make it
    flexible regardless.
    
    I also could have done it like I do with the scipy dct, and use my 1d dct,
    transposing, and adding the correct scaling factor to make it orthonormal. I
    decided against it since I had already gone too far down the path I was already
    on. I may do that later though, because the 1d dct is easy to implement a fft
    for and it may speed up processing time dramatically. Probably would replace
    my 2d dct with scipy's and see before I actually waste what little time I have
    left before this is due.

--- janalysis/dct.py
+++ janalysis/dct.py
@@ -1,5 +1,5 @@
 """Implements the DCT."""
-from math import cos, pi
+from math import cos, pi, sqrt
 import numpy
 
 
@@ -10,7 +10,7 @@ def dct2(x_list, n_point=None):
     :param n_point: The n point size of the DCT to be performed (default None
                     is to use the size of the input).
     :type n_point: int.
-    :returns: array -- the transform representation of the sequence.
+    :returns: numpy.array -- the transform representation of the sequence.
     """
     if not n_point:
         n_point = len(x_list)
@@ -23,3 +23,61 @@ def dct2(x_list, n_point=None):
                 y_array[index_k] += x_array[index_n] * temp
         y_array[index_k] *= 2
     return y_array
+
+
+def dct2_twod_orthonormal(two_d_array):
+    """
+    Implements the 2d dct2 as JPEG requires it (orthonormal, N points' size
+    equal to row and columns).
+    :param two_d_array: The time domain sequence.
+    :type two_d_array: numpy.array.
+    :returns: numpy.array -- the transform representation of the sequence.
+    """
+    matrix_dimensions = two_d_array.shape
+    output = numpy.zeros(matrix_dimensions)
+    for row_index, output_row in enumerate(output):
+        for col_index in range(output_row.size):
+            output[row_index][col_index] = _get_twod_value_dct2(row_index,
+                                                                col_index,
+                                                                two_d_array)
+    return output
+
+
+def _get_twod_value_dct2(output_row_index, output_column_index, two_d_array):
+    """Does the actual computation for each entry in the 2d orthonormal dct.
+    :param output_row_index: The row index of the entry to be calculated
+    :type output_row_index: unsigned int.
+    :param output_column_index: The column index of the entry to be calculated.
+    :type output_column_index: unsigned int.
+    :param two_d_array: The input 2d array that is being transformed.
+    :type two_d_array: numpy.array.
+    :returns: double -- The value calculated for the specific matrix entry.
+    """
+    value = 0
+    row_size = two_d_array.shape[0]
+    column_size = two_d_array.shape[1]
+    for row_index, row in enumerate(two_d_array):
+        for column_index in range(row.size):
+            argx = (row_index+0.5) * output_row_index * pi / row_size
+            argy = (column_index+0.5) * output_column_index * pi / column_size
+            value += two_d_array[row_index][column_index]*cos(argx)*cos(argy)
+    value *= 4  # No idea why this 4 needs to be here, but it does
+    value *= _get_normalization_factor(output_row_index, row_size)
+    value *= _get_normalization_factor(output_column_index, column_size)
+    return value
+
+
+def _get_normalization_factor(index, length):
+    """Returns the normalization factor needed by the 2d dct to be orthonormal.
+    :param index: The index.
+    :type index: unsigned int.
+    :param length: The length.
+    :type length: unsigned int.
+    :returns: double -- The normalization factor for this index.
+    """
+    norm = 0
+    if index == 0:
+        norm = sqrt(1 / (4 * length))
+    else:
+        norm = sqrt(1 / (2 * length))
+    return norm
--- tests/test_dct.py
+++ tests/test_dct.py
@@ -1,10 +1,10 @@
 """Test the DCT implementation."""
 import numpy
 import scipy.fftpack
-from janalysis.dct import dct2
+from janalysis.dct import dct2, dct2_twod_orthonormal
 
 
-def test_1d_dct2_simple():
+def test_dct2_simple():
     """Tests the 1D version of the dct2."""
     time_domain = [1, 1, 1, 1]
     expected = scipy.fftpack.dct(time_domain)
@@ -12,7 +12,7 @@ def test_1d_dct2_simple():
     numpy.testing.assert_array_almost_equal(actual, expected)
 
 
-def test_1d_dct2_simple_specific_length():
+def test_dct2_same_length():
     """Tests the 1D version of the dct2 with n_point of equal length."""
     time_domain = [1, 1, 1, 1]
     expected = scipy.fftpack.dct(time_domain)
@@ -20,7 +20,7 @@ def test_1d_dct2_simple_specific_length():
     numpy.testing.assert_array_almost_equal(actual, expected)
 
 
-def test_1d_dct2_smaller_length():
+def test_dct2_smaller_length():
     """Tests the 1D version of the dct2 with n_point less than input length."""
     time_domain = [1, 1, 1, 1]
     expected = scipy.fftpack.dct(time_domain, n=3)
@@ -33,4 +33,44 @@ def test_dct2_bigger_length():
     time_domain = [1, 1, 1, 1]
     expected = scipy.fftpack.dct(time_domain, n=8)
     actual = dct2(time_domain, 8)
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_twod_dct2_dc_square():
+    """Tests the 2d dct (orthonormalized) on a dc sequence."""
+    time_domain = numpy.array([[1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1],
+                               [1, 1, 1, 1, 1, 1, 1, 1]])
+    expected = scipy.fftpack.dct(scipy.fftpack.dct(time_domain.T, norm='ortho')
+                                 .T, norm='ortho')
+    actual = dct2_twod_orthonormal(time_domain)
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_twod_dct2_smaller_square():
+    """Tests the 2d dct (orthonormalized)."""
+    time_domain = numpy.array([[1, 2, 3, 4],
+                               [4, 3, 2, 1],
+                               [1, 2, 3, 4],
+                               [4, 3, 2, 1]])
+    expected = scipy.fftpack.dct(scipy.fftpack.dct(time_domain.T, norm='ortho')
+                                 .T, norm='ortho')
+    actual = dct2_twod_orthonormal(time_domain)
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+
+def test_twod_dct2_non_square():
+    """Tests the 2d dct (orthonormalized)."""
+    time_domain = numpy.array([[1, 2, 3],
+                               [4, 3, 2],
+                               [1, 2, 3],
+                               [4, 3, 2]])
+    expected = scipy.fftpack.dct(scipy.fftpack.dct(time_domain.T, norm='ortho')
+                                 .T, norm='ortho')
+    actual = dct2_twod_orthonormal(time_domain)
     numpy.testing.assert_array_almost_equal(actual, expected)
