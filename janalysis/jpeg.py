7989238259b08b98736e73920a7db23c093a9603
Andrew Halaney <ajhalaney@gmail.com>
2018-04-17 20:02:20 -0500

    Get file format somewhat correct
    The file format is now correct in most cases, but I seemed to have
    overlooked the stuff byte needed when encoding 0xFF in the scan. I
    believe that's the main problem left with the encoder.

--- janalysis/jpeg.py
+++ janalysis/jpeg.py
@@ -5,7 +5,8 @@ from .huffman import JPEG_HUFFMAN_DC_LUM, JPEG_HUFFMAN_AC_LUM
 from .imageinput import get_image, crop_image_to_multiple_eight, \
                         get_ycbcr_bands, get_pixels, \
                         create_matrices_pixel_sequence
-from .utils import ZIGZAG_ORDER
+from .utils import ZIGZAG_ORDER, get_huffman_table_bit_string, \
+                   get_magnitude_dc, get_ones_complement_bit_string
 
 
 # Make this a command line arg
@@ -37,14 +38,6 @@ def jpeg_encode(input_path):
     chromr_matrices = create_matrices_pixel_sequence(get_pixels(chromr),
                                                      width, height)
 
-    # Confirm everything is 0-255
-    all_matrices = [lum_matrices, chromb_matrices, chromr_matrices]
-    for matrices in all_matrices:
-        for matrix in matrices:
-            for row in matrix:
-                for pixel in row:
-                    assert pixel < 256 and pixel >= 0
-
     # Shift them to be centered around 0
     all_matrices = [lum_matrices, chromb_matrices, chromr_matrices]
     for matrices in all_matrices:
@@ -71,7 +64,7 @@ def jpeg_encode(input_path):
 
     # Subtract DC components
     for matrices in all_matrices:
-        for index, matrix in enumerate(matrices):
+        for index, matrix in reversed(list(enumerate(matrices))):
             if index == 0:
                 continue  # Don't subtract the first DC term
             matrices[index][0][0] = matrix[0][0] - matrices[index - 1][0][0]
@@ -91,10 +84,6 @@ def jpeg_encode(input_path):
         for index in ZIGZAG_ORDER:
             zigzaged_matrix.append(matrix[index[0]][index[1]])
         zigzaged_lists.append(zigzaged_matrix)
-
-    # TODO: Remove sanity check
-    for l in zigzaged_lists:
-        assert len(l) == 64
 
     # Run length encode all
     run_lengthed_lists = []
@@ -137,11 +126,6 @@ def jpeg_encode(input_path):
             serial_index += 1
         run_lengthed_lists.append(run_length)
 
-    # TODO: DETERMINE WHY EIGHTBYEIGHT.PNG OUTPUT SEEMS REALLY WRONG
-
-    # https://www.impulseadventure.com/photo/jpeg-huffman-coding.html
-    # page 102 of textbook
-
     # Huffman encode the whole scan
     for run_length in run_lengthed_lists:
         for index, tup in enumerate(run_length):
@@ -154,75 +138,115 @@ def jpeg_encode(input_path):
                 run_length[index] = (JPEG_HUFFMAN_AC_LUM[tup[0]], tup[1])
 
     # Format file, add DQT, DHT, and scan
-    file_string = bin(0xFFD8)[2:]  # SOI
+    file_string = bin(0xFFD8)[2:].zfill(16)  # SOI
 
-    file_string += bin(0xFFE0)[2:]  # APP0
-    file_string += bin(0x0010)[2:]  # Length of APP0, including the length (16 bytes)
-    file_string += bin(0x4A46494600)[2:]  # 'JFIF\0'
-    file_string += bin(0x0102)[2:]  # JFIF version 1.02
-    file_string += bin(0x01)[2:]  # Units (DPI)
-    file_string += bin(0x0040)[2:]  # Arbitrary X DPI
-    file_string += bin(0x0040)[2:]  # Arbitrary Y DPI
-    file_string += bin(0x00)[2:]  # X thumbnail length
-    file_string += bin(0x00)[2:]  # Y thumbnail length
+    file_string += bin(0xFFE0)[2:].zfill(16)  # APP0
+    file_string += bin(0x0010)[2:].zfill(16)  # Length of APP0, including the length (16 bytes)
+    file_string += bin(0x4A46494600)[2:].zfill(40)  # 'JFIF\0'
+    file_string += bin(0x0102)[2:].zfill(16)  # JFIF version 1.02
+    file_string += bin(0x01)[2:].zfill(8)  # Units (DPI)
+    file_string += bin(0x0040)[2:].zfill(16)  # Arbitrary X DPI
+    file_string += bin(0x0040)[2:].zfill(16)  # Arbitrary Y DPI
+    file_string += bin(0x00)[2:].zfill(8)  # X thumbnail length
+    file_string += bin(0x00)[2:].zfill(8)  # Y thumbnail length
 
     # Encode the quantization table
     dqt = []
     for index in ZIGZAG_ORDER:
         dqt.append(L_QUANTIZATION_TABLE[index[0]][index[1]])
-    file_string += bin(0xFFDB)[2:]  # DQT Marker
-    file_string += bin(0x0043)[2:]  # Length (67), including the length bytes
-    file_string += bin(0x0000)[2:]  # Table value sizes (0 means 1 byte) and table identifier 0
+    file_string += bin(0xFFDB)[2:].zfill(16)  # DQT Marker
+    file_string += bin(0x0043)[2:].zfill(16)  # Length (67), including the length bytes
+    file_string += bin(0x00)[2:].zfill(8)  # Table value sizes (0 means 1 byte) and table identifier 0
     for value in dqt:
         file_string += (bin(value)[2:]).zfill(8)  # Add each table value
-    # TODO: Start of frame
-    # TODO: DHT
-    # TODO: Scan
-    # TODO: EOI ^_^
 
+    # Encode the huffman table
+    # TODO: FIX THIS
+    file_string += bin(0xFFC4)[2:].zfill(16)  # DHT MARKER
 
+    # Create bitstring representation for the huffman tables
+    dc_table_string = bin(0x00)[2:].zfill(8)  # DC table, identifier 0
+    dc_table = get_huffman_table_bit_string(JPEG_HUFFMAN_DC_LUM)
+    ac_table_string = bin(0x10)[2:].zfill(8)  # AC table, identifier 0
+    ac_table = get_huffman_table_bit_string(JPEG_HUFFMAN_AC_LUM)
+
+    # Write the length of the huffman tables plus the 2 bytes of the length bytes
+    length = int(((len(dc_table) + len(ac_table)) / 8)) + 2 + 2
+    dc_table_string += dc_table
+    ac_table_string += ac_table
+    file_string += (bin(length)[2:]).zfill(16)
+
+    # Write the huffman tables
+    file_string += dc_table_string + ac_table_string
+
+    # Start of frame
+    file_string += bin(0xFFC0)[2:].zfill(16)  # SOF0 marker
+    file_string += bin(0x0011)[2:].zfill(16)  # Length
+    file_string += bin(0x08)[2:].zfill(8)  # Sample precision (8 bits)
+    file_string += (bin(height)[2:]).zfill(16)  # 2 bytes for height
+    file_string += (bin(width)[2:]).zfill(16)  # 2 bytes for width
+    file_string += (bin(0x03)[2:]).zfill(8)  # Number of components
+    # Y component for SOF
+    file_string += bin(0x01)[2:].zfill(8)  # 1 is the Y indicator for JFIF files
+    file_string += bin(0x11)[2:].zfill(8)  # Sampling frequency of 1 to 1
+    file_string += bin(0x00)[2:].zfill(8)  # Quantization table identifier
+    # Cb component for SOF
+    file_string += bin(0x02)[2:].zfill(8)  # 2 is the Cb indicator for JFIF files
+    file_string += bin(0x11)[2:].zfill(8)  # Sampling frequency of 1 to 1
+    file_string += bin(0x00)[2:].zfill(8)  # Quantization table identifier
+    # Cr component for SOF
+    file_string += bin(0x03)[2:].zfill(8)  # 3 is the Cb indicator for JFIF files
+    file_string += bin(0x11)[2:].zfill(8)  # Sampling frequency of 1 to 1
+    file_string += bin(0x00)[2:].zfill(8)  # Quantization table identifier
+
+    # Scan
+    file_string += bin(0xFFDA)[2:].zfill(16)  # Scan component
+    file_string += bin(0x000C)[2:].zfill(16)  # Length
+    file_string += bin(0x03)[2:].zfill(8)  # Component count
+    file_string += bin(0x01)[2:].zfill(8)  # Y component
+    file_string += bin(0x00)[2:].zfill(8)  # DC and AC huffman table identifiers
+    file_string += bin(0x02)[2:].zfill(8)  # Cb component
+    file_string += bin(0x00)[2:].zfill(8)  # DC and AC huffman table identifiers
+    file_string += bin(0x03)[2:].zfill(8)  # Cr component
+    file_string += bin(0x00)[2:].zfill(8)  # DC and AC huffman table identifiers
+    file_string += bin(0x00)[2:].zfill(8)  # Spectral selection start
+    file_string += bin(0x3F)[2:].zfill(8)  # Spectral selection end (63)
+    file_string += bin(0x00)[2:].zfill(8)  # Successive approximation
+
+    # Dump the scan data
+    for run_length in run_lengthed_lists:
+        for mag, literal in run_length:
+            file_string += mag + literal
+
+    # Add throw away bits to make it a byte
+    if len(file_string) % 8:
+        for bit in range(8 - len(file_string) % 8):
+            file_string += '1'
+
+    # EOI
+    file_string += bin(0xFFD9)[2:].zfill(16)
+
+    # Split our massive and inefficient bitstring into bytes
+    bytez = [file_string[index:index+8] for
+             index in range(0, len(file_string), 8)]
+    bytez = [int(byte, 2) for byte in bytez]
+
+    # Write all the bites to a file
+    with open('test.jpg', 'wb') as fp:
+        fp.write(bytes(bytez))
 
 
     # Temp print to show this works
     print('{} {} {}'.format(len(lum_matrices), len(chromb_matrices),
                             len(chromr_matrices)))
     print('{}'.format(len(run_lengthed_lists)))
-    print(lum_matrices)
-    print(chromb_matrices)
-    print(chromr_matrices)
-    print(zigzaged_lists)
-    print(run_lengthed_lists)
+    # print(lum_matrices)
+    # print(chromb_matrices)
+    # print(chromr_matrices)
+    # print(zigzaged_lists)
+    # print(run_lengthed_lists)
 
 
-def get_magnitude_dc(value):
-    """Returns the 1 byte magnitude for a DC pixel."""
-    if value < 1:
-        value *= -1
-    length = 0
-    while value:
-        value >>= 1
-        length += 1
-    return length
-
-
-def get_ones_complement_bit_string(value):
-    """Returns the ones complement bit string of a value (TODO: FOR DC ONLY?)"""
-    if value == 0:
-        return ''
-    negative = False
-    if value < 0:
-        negative = True
-        value *= -1
-    bit_string = bin(value)[2:]  # Chop off the '0b' bin returns
-    if negative:
-        bit_list = list(bit_string)
-        for index, bit in enumerate(bit_list):
-            if bit == '0':
-                bit_list[index] = '1'
-            else:
-                bit_list[index] = '0'
-        bit_string = ''.join(bit_list)
-    return bit_string
 
 
 
